5096ba630d30fe328ccb85c1589a84a7bbe6beb1
Wheepsio <90384984+Wheepsio@users.noreply.github.com>
2022-01-20 17:07:24 +0300

    Add files via upload

---
+++ RBPO2-main/Task1/Task1/task1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+#define M_PI
+int main()
+{
+	double x, f;
+	x = 2;
+	std::cout << "x: " << x << std::endl;
+	f = 2 * pow(sin(3 * M_PI - 2 * x), 2) * (pow(cos(5 * M_PI + 2 * x), 2));
+	std::cout << "f = " << f << std::endl;
+	std::cout << "x = ";
+	std::cin >> x;
+	f = 2 * pow(sin(3 * M_PI - 2 * x), 2) * (pow(cos(5 * M_PI + 2 * x), 2));
+	std::cout << "f = " << f << std::endl;
+	return 0;
+}
---
+++ RBPO2-main/task6/task6/task6_func.cpp
@@ -0,0 +1,9 @@
+﻿#include <cmath>
+#define M_PI
+
+double f(double x) {
+    double f;
+    f = 2 * pow(sin(3 * M_PI - 2 * x), 2) * (pow(cos(5 * M_PI + 2 * x), 2));
+    return f;
+}
+
